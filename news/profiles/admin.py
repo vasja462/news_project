0e38850fd7fcd676b60042425ad27b9bbc107334
asvol <a.volosnov@teachbase.ru>
2022-06-28 12:05:12 +0300

    rebuild custom user

--- news/profiles/admin.py
+++ news/profiles/admin.py
@@ -1,15 +1,20 @@
-# from django.contrib import admin
-# from .models import User
-# from django.contrib.auth.admin import UserAdmin
-#
-# @admin.register(User)
-# class UserAd(UserAdmin):
-#     list_display = ['email']
-#     fieldsets = (
-#         ('Персональная информация', {'fields': ('email',)}),
-#     )
-#     search_fields = ('email',)
-#     ordering = ('email',)
-#
-#
-# # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email',)
+    list_filter = ('email',)
+    fieldsets = (
+        ('Personal Information', {'fields': ('email',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', )}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
